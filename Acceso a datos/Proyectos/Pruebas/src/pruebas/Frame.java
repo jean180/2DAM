e6f1456955c3f262038118e5702ee2b221cb8bf8
jean180 <56744601+jean180@users.noreply.github.com>
2020-11-02 14:54:06 +0100

    update

---
+++ Acceso a datos/Proyectos/Pruebas/src/pruebas/Frame.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebas;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+/**
+ *
+ * @author jean_
+ */
+public class Frame extends JFrame {
+
+    private JPanel panelPrincipal;
+    private JRadioButton botonColor;
+
+}
