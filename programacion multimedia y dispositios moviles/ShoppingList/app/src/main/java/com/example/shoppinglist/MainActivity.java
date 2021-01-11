aed5d5eb3fe72bb736d4272a5e855f30617e2688
jeaagu <jean_2_0_0_0_@hotmail.com>
2021-01-11 09:56:04 +0100

    ejercicicio ejemplo lista de compra

---
+++ programacion multimedia y dispositios moviles/ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.shoppinglist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ShoppingListViewModel mViewModel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView dbText = findViewById(R.id.db_text);
+
+        ViewModelProvider.AndroidViewModelFactory factory =
+                ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication());
+
+        mViewModel = new ViewModelProvider(this, factory)
+                .get(ShoppingListViewModel.class);
+
+        mViewModel.getShoppingLists().observe(this, shoppingLists -> {
+            StringBuilder sb = new StringBuilder();
+            for (ShoppingList list : shoppingLists){
+                sb.append(list.getName()).append("\n");
+            }
+            dbText.setText(sb.toString());
+        });
+    }
+}
---
+++ programacion multimedia y dispositios moviles/ShoppingList/app/src/main/java/com/example/shoppinglist/ShoppingListViewModel.java
@@ -0,0 +1,30 @@
+package com.example.shoppinglist;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class ShoppingListViewModel extends AndroidViewModel {
+
+    private final ShoppingListRepository mRepository;
+
+    private final LiveData<List<ShoppingList>> mShoppingLists;
+
+    public ShoppingListViewModel(@NonNull Application application){
+        super(application);
+        mRepository = new ShoppingListRepository(application);
+        mShoppingLists = mRepository.getAllShoppingLists();
+    }
+
+    public LiveData<List<ShoppingList>> getShoppingLists() {
+        return mShoppingLists;
+    }
+
+    public void insert(ShoppingList shoppingList){
+        mRepository.insert(shoppingList);
+    }
+}
